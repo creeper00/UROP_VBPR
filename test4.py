3d45d185cf473e4e00e2aba1dac921889db7b9e5
creeper00 <rnjswldnd@snu.ac.kr>
2021-11-05 18:36:42 +0900

    test4

--- test4.py
+++ test4.py
@@ -13,6 +13,8 @@ import json
 import ast
 import gzip
 import array
+import torch.utils.data as data
+import torch
 
 print(0)
 model = EfficientNet.from_pretrained('efficientnet-b0')
@@ -24,36 +26,73 @@ def parse(path):
   for l in g:
     yield json.dumps(eval(l))
 
-print(3)
-x = 0
-file = open("../datasets/efficient_Cell_Phones_and_Accessories.b", "wb")
-tfms1 = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor(),
+tfms1 = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),])
 tfms2 = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor(),
     transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),])
-for i in parse(p) :
-    x=x+1
-    j = json.loads(i)
-    try:
-        url = j['imUrl']
-        response = requests.get(url)
-        img = Image.open(BytesIO(response.content))
-    except:
-        print(x)
-        continue
-    try:
-        img = tfms1(img).unsqueeze(0)
-    except:
-        img = tfms2(img).unsqueeze(0)
-    features = model.extract_features(img)
-    m = nn.Linear(81920, 4096)
-    f = m(features.view(-1))
-    pid = j['asin'].encode()
-    data = (pid, f.detach().numpy().tolist())
-    file.write(pid)
-    fl = f.detach().numpy().tolist()
-    file.write(bytes(array.array('f', fl)))
 
+def loader() :
+    predata = []
+    x=0
+    for i in parse(p) :
+        x=x+1
+        if x==100 : break
+        j = json.loads(i)
+        try:
+            url = j['imUrl']
+            response = requests.get(url)
+            img = Image.open(BytesIO(response.content))
+        except:
+            print(x)
+            continue
+        try:
+            img = tfms1(img).unsqueeze(0)
+        except:
+            img = tfms2(img).unsqueeze(0)
+        pid = j['asin'].encode()
+        tup = (pid, img)
+        predata.append(tup)
+    return predata
 
+class ImgDataset(data.Dataset):
 
+    def __init__(self, img_list):
+        self.img_list = img_list
+
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, index):
+        img = self.img_list[index]
+
+        return img
+
+class ExtractEfficientNet(nn.Module) :
+    def __init__(self):
+         super(ExtractEfficientNet, self).__init__()
+         self.features = model.extract_features
+    def forward(self, x):
+        x = self.features(x)
+        return x
+
+device = torch.device("cuda")
+print(1)
+pdataset = ImgDataset(img_list=loader())
+print(2)
+pdataloader = data.DataLoader(pdataset, batch_size=32, shuffle=False)
+
+nmodel = ExtractEfficientNet()
+print(3)
+
+file = open("../datasets/efficient_Cell_Phones_and_Accessories.b", "wb")
+for batch in pdataloader :
+    nmodel.to(device)
+    for i in range(0, len(batch[0])):
+        feat = nmodel(batch[1][i])
+        m = nn.Linear(62720, 4096)
+        f = m(feat.view(-1))
+        fl = f.detach().numpy().tolist()
+        file.write(batch[0][i])
+        file.write(bytes(array.array('f', fl)))
+file.close()
