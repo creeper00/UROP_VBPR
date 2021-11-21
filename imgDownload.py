20d251a6eaa32b74f9e03b999d978780b164f39a
Jiwoong Kwon <43807159+creeper00@users.noreply.github.com>
2021-11-21 21:05:17 +0900

    Update imgDownload.py

--- imgDownload.py
+++ imgDownload.py
@@ -1,9 +1,11 @@
-import urllib.request
+import concurrent.futures
+import os
+import requests
 import json
 from pathlib import Path
 import gzip
 
-p = Path("../../Downloads/meta_Cell_Phones_and_Accessories.json.gz")
+p = Path("../../Downloads/meta_Clothing_Shoes_and_Jewelry.json.gz")
 
 def parse(path):
   g = gzip.open(path, 'r')
@@ -11,14 +13,36 @@ def parse(path):
     yield json.dumps(eval(l))
 
 def loader() :    
+    predata = []
     for i in parse(p) :
         j = json.loads(i)
         try:
             url = j['imUrl']
             pid = j['asin']
-            name = pid+".jpg"
-            urllib.request.urlretrieve(url, name)
+            predata.append((pid,url))
         except:
             continue
+    return predata
 
-loader()
+def save_image_from_url(url):
+    image = requests.get(url[1])
+    with open(Path(url[0]+".jpg"), "wb") as f:
+        f.write(image.content)
+
+def load(dt):    
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=5
+    ) as executor:
+        future_to_url = {
+            executor.submit(save_image_from_url, url): url
+            for url in dt
+        }
+        for future in concurrent.futures.as_completed(
+            future_to_url
+        ):
+            url = future_to_url[future]
+            try:
+                future.result()
+            except Exception as exc:
+                print(1)
+load(loader())
